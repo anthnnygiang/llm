580508bc25e9d22730dfb96bad4d02e6f7b8bebf
Anthony Giang <anth@sent.com>
2023-07-03 01:02:28 +1000

    Add: better interactive prompt

--- ai.mjs
+++ ai.mjs
@@ -15,24 +15,21 @@ program
   .option("-d, --debug", "output debug information", false)
   .option("-i, --interactive", "interactive prompt", false)
   .option("-s, --stream", "stream text or not", false)
-  .option("-t, --temperature <temperature>", "temperature longer longer argument", parseFloat, 1)
-  .option("-p, --presence-penalty <present-penalty>", "presence argument", parseFloat, 1)
-  .option("-f, --frequency-penalty <frequency-penalty>", "frequency argument", parseFloat, 1);
+  .option("-t, --temperature <temperature>", "temperature longer longer argument", parseFloat, 1);
 /* arguments originate from node */
 program.parse(process.argv);
-const { interactive, stream, temperature, presencePenalty, frequencyPenalty } = program.opts();
+const { interactive, stream, temperature } = program.opts();
+const content = program.args.join(" ");
 
 /*******************/
-/* NETWORK REQUEST */
+/* ENDPOINT REQUEST */
 
-async function chat({ messages, stream, temperature, presencePenalty, frequencyPenalty }) {
+async function chat({ messages, stream, temperature }) {
   const output = await openai.createChatCompletion({
     model: MODEL,
     messages: messages,
     stream: stream,
     temperature: temperature,
-    presence_penalty: presencePenalty,
-    frequency_penalty: frequencyPenalty,
   });
   console.log(output.data.choices[0].message.content); /* print to stdout */
   return output.data.choices[0].message;
@@ -46,12 +43,11 @@ const openai = new OpenAIApi(configuration);
 /* SINGLE PROMPT */
 
 if (!interactive) {
-  const content = program.args.join(" ");
   if (content === "") {
     console.log("Invalid input.");
   } else {
     const messages = [{ role: "user", content: content }];
-    await chat({ messages, interactive, stream, temperature, presencePenalty, frequencyPenalty });
+    await chat({ messages, interactive, stream, temperature });
   }
   process.exit(0);
 }
@@ -61,6 +57,11 @@ if (!interactive) {
 
 if (interactive) {
   const messages = [];
+  if (content !== "") {
+    messages.push({ role: "user", content: content });
+    await chat({ messages, interactive, stream, temperature });
+  }
+
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
@@ -74,12 +75,8 @@ if (interactive) {
       console.log("Invalid input.");
       rl.prompt();
     }
-    const nextMessage = {
-      role: "user",
-      content: line,
-    };
-    messages.push(nextMessage);
-    await chat({ messages, interactive, stream, temperature, presencePenalty, frequencyPenalty });
+    messages.push({ role: "user", content: line });
+    await chat({ messages, interactive, stream, temperature });
     rl.prompt();
   }).on("close", () => {
     console.log("\nInteractive mode closed.");
